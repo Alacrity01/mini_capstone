ac6886bf8840fccd15b17d2390870bd37d7b9952
TheAlacrity <brinker.jeff@gmail.com>
2019-05-06 11:42:39 -0500

    added cors.rb to API; fixed routes URL names

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/products_controller' => 'products#index'
-    post '/products_controller' => 'products#create'
-    get '/products_controller/:id' => 'products#show'
-    patch '/products_controller/:id' => 'products#update'
-    delete 'products_controller/:id' => 'products#destroy'
+    get '/products' => 'products#index'
+    post '/products' => 'products#create'
+    get '/products/:id' => 'products#show'
+    patch '/products/:id' => 'products#update'
+    delete 'products/:id' => 'products#destroy'
   end
 end
