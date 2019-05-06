fbd52f70222cb8551637024be76a67db7baf7aae
TheAlacrity <brinker.jeff@gmail.com>
2019-05-06 11:03:19 -0500

    added some function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    get '/products_controller/index' => 'products#index'
+    get '/products_controller' => 'products#index'
+    post '/products_controller' => 'products#create'
     get '/products_controller/:id' => 'products#show'
   end
 end
