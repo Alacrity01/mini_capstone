f8350d5f0a0da6ec642275062ccdb9bf7930e35d
TheAlacrity <brinker.jeff@gmail.com>
2019-05-10 17:00:27 -0500

    added supplier model and supplier IDs to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# supplier = Supplier.new(name: "Google", email: "google@google.com", phone_number: "302-555-9111")
+# supplier.save
+
+# supplier = Supplier.new(name: "Amazon", email: "amazon@amazon.com", phone_number: "303-555-9191")
+# supplier.save
+
+# supplier = Supplier.new(name: "Mozilla", email: "mozilla@firefox.com", phone_number: "304-555-9119")
+# supplier.save
+
+# id_array = [3, 5, 6, 4, 7, 1, 10, 11, 2]
+# supplier_array = [1, 2, 3]
+# products = Product.all
+
+# products.each do |product|
+#   product.supplier_id = supplier_array.sample
+#   product.save
+# end
